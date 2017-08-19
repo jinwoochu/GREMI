5fb33a1c24b313780cd76dc85e040b1ccd6c6b7c
Jeon-jinhyeong <jinhyeong9905@gmail.com>
2017-08-19 15:32:54 +0900

    프로필 지도 고정

--- public/js/sell/sell.js
+++ public/js/sell/sell.js
@@ -5,50 +5,18 @@ var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 function initMap() {
   map = new google.maps.Map(
     document.getElementById('map'), {
-      zoom: 12,
-      center: {lat: -34.397, lng: 150.644}
+      zoom: 2,
+      center: {lat: 37.250943, lng: 127.028344},
+      panControl: false,
+      zoomControl: false,
+      scaleConrtol: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      overviewMapControl: false,
+      scrollwheel: false,
+      draggable: false
     });
-
-  var geocoder = new google.maps.Geocoder();
-
-  $('#submit').on('click', function() {
-    geocodeAddress(geocoder, map);
-  }); 
-
-  $('#address').on('keypress', function(event) {
-    if((event.keyCode ? event.keyCode : event.which) == 13){
-      geocodeAddress(geocoder, map);
-    }
-  });
 }
-
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-
-      var myCity = new google.maps.Circle({
-        center: results[0].geometry.location,
-        radius:800,
-        strokeColor:"#0000FF",
-        strokeOpacity:0.8,
-        strokeWeight:2,
-        fillOpacity:0.0,
-        map: resultsMap
-      });
-      var cityCircle = new google.maps.Circle(myCity);
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
-
-
 
 $(document).ready(function() {
   $('#investment_menu').on('click', 'a', function(event) {
